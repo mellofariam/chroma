23fb1e0749b51baaa7a99513cbc084f4d5aeffa1
Matheus Mello <mfmello@outlook.com>
2023-07-28 13:18:07 -0500

    feat: cndb2xtc works without top file

--- convert.py
+++ convert.py
@@ -15,7 +15,39 @@ def cndb2xtc(cndb_file: str, topfile: str, output: str = None, frames=[1, None, 
     reader.load(filename=cndb_file)
     positions = reader.xyz(frames=frames)
 
-    traj = md.load(topfile)
+    if topfile is None:
+        annotations = reader.ChromSeq
+        chr2aa = {
+            "A1": "ASP",
+            "A2": "GLU",
+            "B1": "ARG",
+            "B2": "LYS",
+            "B3": "HIS",
+            "B4": "HIS",
+            "NA": "GLY",
+        }
+
+        with open(output + ".pdb", "w") as pdb_file:
+            for i in range(len(positions[0])):
+                j = ["" for _ in range(9)]
+                j[0] = "ATOM".ljust(6)  # atom#6s
+                j[1] = str(i + 1).rjust(5)  # aomnum#5d
+                j[2] = "CA".center(4)  # atomname$#4s
+                j[3] = chr2aa[annotations[i]].ljust(3)  # resname#1s
+                j[4] = "A".rjust(1)  # Astring
+                j[5] = str(i + 1).rjust(4)  # resnum
+                j[6] = str("%8.3f" % (float(positions[0][i][0]))).rjust(8)  # x
+                j[7] = str("%8.3f" % (float(positions[0][i][1]))).rjust(8)  # y
+                j[8] = str("%8.3f" % (float(positions[0][i][2]))).rjust(8)  # z
+                pdb_file.write(
+                    "{}{} {} {} {}{}    {}{}{}\n".format(
+                        j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8]
+                    )
+                )
+            pdb_file.write("END")
+        traj = md.load(output + ".pdb")
+    else:
+        traj = md.load(topfile)
 
     if extension != ".gro":
         traj.xyz = positions / 10
