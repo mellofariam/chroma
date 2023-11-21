88a86d3f6b317151fd7d0186dce1ebe0dcc6eefe
Matheus Mello <mfmello@outlook.com>
2023-11-21 15:13:04 -0600

    feat: function to build mdtraj topology and to with pdb from xyz coords

--- convert.py
+++ convert.py
@@ -5,10 +5,14 @@ import mdtraj as md
 import OpenMiChroM.CndbTools as ctools
 
 
-def cndb2xtc(cndb_file: str, topfile: str = None, output: str = None, frames=[1, None, 1]):
-    if output is None:
-        output, _ = os.path.splitext(cndb_file)
-
+def cndb2xtc(
+    cndb_file: str,
+    topfile: str = None,
+    filename: str = None,
+    frames=[1, None, 1],
+):
+    if filename is None:
+        filename, _ = os.path.splitext(cndb_file)
 
     reader = ctools.cndbTools()
     reader.load(filename=cndb_file)
@@ -26,30 +30,40 @@ def cndb2xtc(cndb_file: str, topfile: str = None, output: str = None, frames=[1,
             "NA": "GLY",
         }
 
-        with open(output + ".pdb", "w") as pdb_file:
-            for i in range(len(positions[0])):
+        with open(filename + ".pdb", "w") as pdb_file:
+            for i in range(len(xyz)):
                 j = ["" for _ in range(9)]
                 j[0] = "ATOM".ljust(6)  # atom#6s
                 j[1] = str(i + 1).rjust(5)  # aomnum#5d
                 j[2] = "CA".center(4)  # atomname$#4s
 
                 try:
-                    j[3] = chr2aa[annotations[i]].ljust(3)  # resname#1s
+                    j[3] = chr2aa[annotations[i]].ljust(
+                        3
+                    )  # resname#1s
                 except:
                     j[3] = "ALA".ljust(3)
 
                 j[4] = "A".rjust(1)  # Astring
                 j[5] = str(i + 1).rjust(4)  # resnum
-                j[6] = str("%8.3f" % (float(positions[0][i][0]))).rjust(8)  # x
-                j[7] = str("%8.3f" % (float(positions[0][i][1]))).rjust(8)  # y
-                j[8] = str("%8.3f" % (float(positions[0][i][2]))).rjust(8)  # z
+                j[6] = str("%8.3f" % (float(xyz[i][0]))).rjust(8)  # x
+                j[7] = str("%8.3f" % (float(xyz[i][1]))).rjust(8)  # y
+                j[8] = str("%8.3f" % (float(xyz[i][2]))).rjust(8)  # z
                 pdb_file.write(
                     "{}{} {} {} {}{}    {}{}{}\n".format(
-                        j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8]
+                        j[0],
+                        j[1],
+                        j[2],
+                        j[3],
+                        j[4],
+                        j[5],
+                        j[6],
+                        j[7],
+                        j[8],
                     )
                 )
             pdb_file.write("END")
-        traj = md.load(output + ".pdb")
+        traj = md.load(filename + ".pdb")
         extension = ".pdb"
     else:
         _, extension = os.path.splitext(topfile)
@@ -61,4 +75,58 @@ def cndb2xtc(cndb_file: str, topfile: str = None, output: str = None, frames=[1,
         traj.xyz = positions
 
     traj.time = np.arange(0, 0.002 * len(positions), 0.002)
-    traj.save_xtc(output + ".xtc")
+    traj.save_xtc(filename + ".xtc")
+
+
+def build_md_topology(chain_length):
+    """
+    Creates the topology object to mdtraj for a chromosome, considering it as
+    a sequence of CA, in which each bead is a residue.
+    """
+    topology = md.Topology()
+    topology.add_chain()
+    for i in range(chain_length):
+        topology.add_residue(name="ALA", chain=topology.chain(0))
+        topology.insert_atom(
+            "CA", md.element.carbon, topology.residue(i)
+        )
+
+        try:
+            topology.add_bond(topology.atom(i), topology.atom(i - 1))
+        except ValueError:
+            pass
+    return topology
+
+
+def write_pdb_from_xyz(xyz, filename="frame", resseq=None):
+    with open(filename + ".pdb", "w") as pdb_file:
+        for i in range(len(xyz)):
+            j = ["" for _ in range(9)]
+            j[0] = "ATOM".ljust(6)  # atom#6s
+            j[1] = str(i + 1).rjust(5)  # aomnum#5d
+            j[2] = "CA".center(4)  # atomname$#4s
+
+            if resseq is None:
+                j[3] = "ALA".ljust(3)
+            else:
+                j[3] = resseq[i].ljust(3)  # resname#1s
+
+            j[4] = "A".rjust(1)  # Astring
+            j[5] = str(i + 1).rjust(4)  # resnum
+            j[6] = str("%8.3f" % (float(xyz[i][0]))).rjust(8)  # x
+            j[7] = str("%8.3f" % (float(xyz[i][1]))).rjust(8)  # y
+            j[8] = str("%8.3f" % (float(xyz[i][2]))).rjust(8)  # z
+            pdb_file.write(
+                "{}{} {} {} {}{}    {}{}{}\n".format(
+                    j[0],
+                    j[1],
+                    j[2],
+                    j[3],
+                    j[4],
+                    j[5],
+                    j[6],
+                    j[7],
+                    j[8],
+                )
+            )
+        pdb_file.write("END")
