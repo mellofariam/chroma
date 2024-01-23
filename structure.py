7ff94ab3c5e3adc5b5bfede34964e0d67562960d
Matheus Mello <mfmello@outlook.com>
2024-01-23 17:00:20 -0600

    feat: add distance_to_center function to compute distance to whole genome CoM

--- structure.py
+++ structure.py
@@ -74,6 +74,77 @@ def lamina_distance(
     return distances
 
 
+def distance_to_center(
+    nucleus_path: str,
+    mode: str = "compartments",
+    frames: range = None,
+    chr_range: range = range(1, 47),
+):
+    traj_files = {
+        chr: h5py.File(f"{nucleus_path}/chromosome{chr}.cndb", "r")
+        for chr in chr_range
+    }
+
+    if frames is None:
+        frames = range(1, len(traj_files[chr_range[0]].keys()), 1)
+
+    subcompartments = []
+    for chr in chr_range:
+        subcompartments.extend(
+            [annot for annot in traj_files[chr]["types"].asstr()]
+        )
+
+    if mode == "compartments":
+        index = {"A": [], "B": [], "NA": []}
+        to_compt = {
+            "A1": "A",
+            "A2": "A",
+            "B1": "B",
+            "B2": "B",
+            "B3": "B",
+            "B4": "B",
+            "NA": "NA",
+        }
+        for idx, annot in enumerate(subcompartments):
+            index[to_compt[annot]].append(idx)
+
+    elif mode == "subcompartments":
+        index = {
+            "A1": [],
+            "A2": [],
+            "B1": [],
+            "B2": [],
+            "B3": [],
+            "B4": [],
+            "NA": [],
+        }
+        for idx, annot in enumerate(subcompartments):
+            index[annot].append(idx)
+
+    distances = {}
+    for key in index.keys():
+        distances[key] = []
+
+    for n, frame in enumerate(frames):
+        if n % 100 == 0:
+            print(f"Analyzing frame {frame}")
+        frame_nucleus = np.concatenate(
+            [
+                np.array(traj_files[chr][str(frame)])
+                for chr in chr_range
+            ]
+        )
+
+        distance_to_center = np.linalg.norm(
+            frame_nucleus - np.mean(frame_nucleus, axis=0), axis=1
+        )
+
+        for key in index.keys():
+            distances[key].extend(distance_to_center[index[key]])
+
+    return distances
+
+
 def radial_density(
     nucleus_path: str,
     chr_filename: str = "nucleus_",
